a939b1c189a64e16c9ca2e907dac52d7a4a88750
Coding <Coding@Teds-Air.lan>
2023-05-20 00:13:47 -0500

    Complete File

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (apiPath, queryTerm="") => {
+    const [data, setData] = useState([]);
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=9ef72c7118eba33a2fb66d3efb85a22f&query=${queryTerm}`
+
+    useEffect(() => {
+        async function fetchMovies(){
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json.results);
+        }
+        fetchMovies();
+      }, [url])
+
+  return { data }
+}
